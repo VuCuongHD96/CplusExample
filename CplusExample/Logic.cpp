b84a5a36abcadd39f8b8a43ab8b63c900a7fe042
VuCuongHD96 <xuancuonghd1102@gmail.com>
2024-03-13 17:04:45 +0700

    import c plus

---
+++ CplusExample/Logic.cpp
@@ -0,0 +1,8 @@
+//
+//  Logic.cpp
+//  CplusExample
+//
+//  Created by Work on 13/03/2024.
+//
+
+#include "Logic.h"
---
+++ CplusExample/Logic.h
@@ -0,0 +1,15 @@
+//
+//  Logic.hpp
+//  CplusExample
+//
+//  Created by Work on 13/03/2024.
+//
+
+// Logic.hpp
+
+#ifndef Logic_hpp
+#define Logic_hpp
+
+#include <stdio.h>
+
+#endif /* Logic_hpp */
